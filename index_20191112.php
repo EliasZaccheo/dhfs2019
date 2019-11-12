a76ab61f1d3c6894f1a202322e7a54645e7bca24
EliasZaccheo <37513041+EliasZaccheo@users.noreply.github.com>
2019-11-12 08:46:45 -0300

    Pre-clase 12/11/2019

--- index_20191112.php
+++ index_20191112.php
@@ -8,3 +8,54 @@
     return $array;
   }
  ?>
+
+<?php
+  function dobles(){
+    $array=unoAlDiez();
+    for ($i=0; $i < 10; $i++) {
+      // code...
+      $array[$i]=$array[$i]*2;
+    }
+    return $array;
+  }
+ ?>
+
+<?php
+  function dobles($array){
+    foreach ($array as $element => $value) {
+      $array[$element]=$value*2;
+    }
+    return $array;
+  }
+?>
+
+<?php
+  function nombreCompleto ($nombre, $apellido){
+    return "$nombre $apellido";
+  }
+ ?>
+
+<?php
+  function entre0Y100($numero){
+    return $numero>=0 && $numero<=100;
+  }
+ ?>
+
+ <?php
+ function entreDosNumeros($elemento, $cotaMenor, $cotaMayor){
+   return $elemento>=$cotaMenor && $elemento<=$cotaMayor;
+ }
+  ?>
+
+<?php
+  function entreDosNumeros($elemento, $cotaMenor=0, $cotaMayor=100){
+    return $elemento>=$cotaMenor && $elemento<=$cotaMayor;
+  }
+ ?>
+
+<?php
+  function aumentarContador(){
+    global $contadorGlobal;
+    $contadorGlobal++;
+  }
+ ?>
